39d729bf4ce533b70ba06f3086b84c336a452006
Felipe Carrion <af.carrion@uniandes.edu.co>
2020-08-03 00:00:28 -0500

    Carga de imagenes

--- src/controllers/image.js
+++ src/controllers/image.js
@@ -1,11 +1,24 @@
+const { randomName } = require('../helpers/libs');
+const path = require('path');
+const fs = require('fs-extra');
 const ctrl = {};
 
 ctrl.index = (req, res) => {
 
 };
 
-ctrl.create = (req, res) => {
-    
+ctrl.create = async (req, res) => {
+    //console.log(req.file);
+    const nameImage = randomName();
+    //console.log(nameImage);
+    const ext = path.extname(req.file.originalname).toLowerCase();
+    const imageTempPath = req.file.path
+    const targetPath = path.resolve(`src/public/upload/${nameImage}${ext}`)
+    //console.log(targetPath);
+    if(ext === '.png' || ext === '.jpg' || ext === '.jpeg' || ext === '.gif'){
+        await fs.rename(imageTempPath, targetPath);
+    }
+    res.send('It works');
 };
 
 ctrl.like = (req, res) => {
